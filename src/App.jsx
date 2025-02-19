983b3cd7c8e05aa6d68539478d38c2d84937d8ed
Optimus Elikem <portiaadaletey@gmail.com>
2025-02-19 16:32:20 +0000

    starter components created for the home page, all images imported into a constant afor accessibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+// import { useState } from 'react'
+import Home from "./Pages/Home/Home";
 
 
 function App() {
---
+++ src/assets/assets.js
@@ -0,0 +1,71 @@
+// Icon imports
+import cart_icon from './cart-icon.png'
+import search_icon from './search-icon (2).png'
+import question_icon from './question-icon.png'
+
+
+// Hero images
+import hero_1 from './Hero-1.png'
+import hero_2 from './Hero-2.png'
+import hero_3 from './Hero-3.png'
+
+
+
+// Testimonials image imports
+import testimony_0 from './smiling-guy.png'
+import testimony_1 from './guy-in-blue.png'
+import testimony_2 from './Lady-2.png'
+import testimony_3 from './actor-guy.png'
+import testimony_4 from './serious-guy.png'
+import testimony_5 from './rasta-guy.png'
+import testimony_6 from './freedom-guy.png'
+
+
+
+// Logos of brands sold importes here
+import sponsor_0 from './maxhub-logo.png'
+import sponsor_1 from './itel-logo.png'
+import sponsor_2 from './akai-logo.png'
+import sponsor_3 from './datalogic-logo.png'
+import sponsor_4 from './sony-logo 1.png'
+import sponsor_5 from './htc-logo.png'
+import sponsor_6 from './siemens-logo 1.png'
+import sponsor_7 from './sandisk-logo_brandlogos.net_kbn6y 1.png'
+import sponsor_8 from './logitech_g-logo_brandlogos.net_tzeki 1.png'
+import sponsor_9 from './osram-vector-logo 1.png'
+
+// Social media icons
+import facebook_icon from './facebook-icon.png'
+import instagram_icon from './insta-icon.png'
+import ticktok_icon from './ticktok_icon'
+import x_icon from './x_icon'
+
+export const assest = {
+    cart_icon,
+    search_icon,
+    question_icon,
+    hero_1,
+    hero_2,
+    hero_3,
+    testimony_0,
+    testimony_1,
+    testimony_2,
+    testimony_3,
+    testimony_4,
+    testimony_5,
+    testimony_6,
+    sponsor_0,
+    sponsor_1,
+    sponsor_2,
+    sponsor_3,
+    sponsor_4,
+    sponsor_5,
+    sponsor_6,
+    sponsor_7,
+    sponsor_8,
+    sponsor_9,
+    facebook_icon,
+    instagram_icon,
+    ticktok_icon,
+    x_icon
+}

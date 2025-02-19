1c1cd288e89e8be9603f678103ed29ddcc7b62f2
Optimus Elikem <portiaadaletey@gmail.com>
2025-02-19 18:22:34 +0000

    started second part of the header

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,16 +4,27 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <header className="header">
-      <div className="logo-area">
-        <p className="logo theme">LOGO</p>
-        <span>Sell on PawnPal</span>
+      <div className="header-top">
+        <div className="logo-area">
+          <p className="logo theme">LOGO</p>
+          <span>Sell on PawnPal</span>
+        </div>
+        <div className="cto">
+          <img
+            src={assets.telephone_icon}
+            alt="telephone icon"
+            className="theme"
+          />
+          <span>Call to Order</span>
+        </div>
+        <div className="tease">
+          <p>Hot deals just for you | We got you covered | Falaa deals</p>
+        </div>
       </div>
-      <div className="cto">
-        <img src={assets.telephone_icon} alt="telephone icon" className="theme"/>
-        <span>Call to Order</span>
-      </div>
-      <div className="tease">
-        <p>Hot deals just for you | We got you covered | Falaa deals</p>
+
+      <div className="header-main">
+        <h2>PAWN <span className="make-orange">PAL</span></h2>
+        <input type="text" />
       </div>
     </header>
   );

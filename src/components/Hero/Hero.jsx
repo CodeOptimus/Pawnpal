cbaa4a5ea92f3fa8e6734fa27fee0b747d608b0d
Romeo Gborgblorvor <colbby@gmail.com>
2025-02-20 09:58:45 -0400

    completed the navbar and added the hero component

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "./Hero.css";
+
+function Hero() {
+  return (
+    <>
+      <div className="item-list">
+        <ul>
+          <li>
+            <a href="#">Phone & Tablets</a>
+          </li>
+          <li>
+            <a href="#">Electronics</a>
+          </li>
+          <li>
+            <a href="#">Computer</a>
+          </li>
+          <li>
+            <a href="#">Clothes</a>
+          </li>
+          <li>
+            <a href="#">Shoes</a>
+          </li>
+          <li>
+            <a href="#">Bags</a>
+          </li>
+          <li>
+            <a href="#">Motor</a>
+          </li>
+          <li>
+            <a href="#">Bicycles</a>
+          </li>
+          <li>
+            <a href="#">Furniture</a>
+          </li>
+          <li>
+            <a href="#">Generator</a>
+          </li>
+          <li>
+            <a href="#">Polytank</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="hero"></div>
+    </>
+  );
+}
+
+export default Hero;

8fa7ff8d4424524526f87496ae1d858b4a170a66
Optimus Elikem <portiaadaletey@gmail.com>
2025-03-04 06:46:45 +0000

    added slider effect for the hero banners

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import { Component } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "./Hero.css";
 
+
+// slider arrow customizations
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "#FE6C2C" }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "#FE6C2C"}}
+      onClick={onClick}
+    />
+  );
+}
+
 function Hero() {
+  // settings for slider effect
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    cssEase: "linear",
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />
+  };
+  
   return (
     <>
       <div className="item-list">
@@ -42,7 +83,21 @@ function Hero() {
         </ul>
       </div>
 
-      <div className="hero"></div>
+      
+  {/* Slider implementation for hero banners */}
+      <div className="slider-container">
+        <Slider {...settings}>
+          <div className="banner">
+            <img src="../../../Hero-1.png" alt="Welcome to Pawnpal banner" />
+          </div>
+          <div className="banner">
+            <img src="../../../Hero-2.png" alt="Black Friday Sales Banner" />
+          </div>
+          <div className="banner">
+            <img src="../../../Hero-3.png" alt="Week's Special Banner" />
+          </div>
+        </Slider>
+      </div>
     </>
   );
 }
